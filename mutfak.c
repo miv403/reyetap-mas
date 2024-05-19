3474fbf934e79d9ace92a7d737cd307f6d461cd1
Melih Var <varmelih711@gmail.com>
2024-05-20 02:20:40 +0300

    cherry-pick: rewrited mutfak.c
    melihi(4a3a4da0) -> miv403-1
    
    ---
    
    mutfak.c tamamen tekrar elden geçirildi

--- mutfak.c
+++ mutfak.c
@@ -1,28 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h> // Unix sistemlerinde sleep fonksiyonu icin
 
 #define YEMEK_UZUNLUK 50
-#define ASCILAR 3 // 3 asciya sahip bir restorani varsayalım
+#define ASCILAR 3 // 3 aşçı varsayımı
 #define YEMEK_LISTESI "./veri/yemeklistesi.txt"
 #define SIPARIS "./veri/siparisler.txt"
 
-
-// Yemek struct'i tanimla 
+// Yemek struct'ı
 typedef struct {
     char ad[YEMEK_UZUNLUK];
     int hazirlanmaSuresi;
 } Yemek;
 
-// Asci struct'i tanimla
+// Aşçı struct'ı
 typedef struct {
     int id;
     int hazirlanacakSure;
+    char yemek[YEMEK_UZUNLUK]; // Aşçının hazırladığı yemeği tutmak için
 } Asci;
 
-// Yemek listesini dosyadan okuyan ve depolayan fonksiyon
-void siparisleriOkuyucu(Yemek yemekler[], int *yemekSayisi) {
+// Yemek listesini okuyan fonksiyon
+void yemekListesiniOku(Yemek yemekler[]) {
     FILE *dosya = fopen(YEMEK_LISTESI, "r");
     if (dosya == NULL) {
         printf("Yemek listesi dosyasi bulunamadi.\n");
@@ -30,22 +29,19 @@ void siparisleriOkuyucu(Yemek yemekler[], int *yemekSayisi) {
     }
 
     char satir[256];
-    while (fgets(satir, sizeof(satir), dosya) != NULL) {
-        char *ad = strtok(satir, "/");
-        char *suresi = strtok(NULL, "/");
-        suresi = strtok(suresi, "\n");
+    int yemekSayisi = 0;
+    fgets(satir, sizeof(satir), dosya); // Başlık satırını atla
 
-        strcpy(yemekler[*yemekSayisi].ad, ad);
-        yemekler[*yemekSayisi].hazirlanmaSuresi = atoi(suresi);
-
-        (*yemekSayisi)++;
+    while (fgets(satir, sizeof(satir), dosya) != NULL && yemekSayisi < YEMEK_UZUNLUK) {
+        sscanf(satir, "%[^|] | %*[^|] | %d | %*[^|]", yemekler[yemekSayisi].ad, &yemekler[yemekSayisi].hazirlanmaSuresi);
+        yemekSayisi++;
     }
 
     fclose(dosya);
 }
 
-// Siparisleri isleyen ana fonksiyon
-void siparisleriIsleyici() {
+// Siparişleri işleyen fonksiyon
+void siparisleriIsle(Yemek yemekler[], Asci ascilar[]) {
     while (1) {
         FILE *dosya = fopen(SIPARIS, "r+");
         if (dosya == NULL) {
@@ -55,54 +51,67 @@ void siparisleriIsleyici() {
 
         char satir[256];
         while (fgets(satir, sizeof(satir), dosya) != NULL) {
-            char *durum = strtok(satir, ",");
-            if (strcmp(durum, "1") == 0) {
-                // Siparis hazirlaniyor
-                char *siparisID = strtok(NULL, "|");
-                char *kullaniciAdi = strtok(NULL, "|");
+            // Sadece onaylanmış (durum 1) siparişleri al
+            if (satir[0] == '1') {
+                strtok(satir, "|"); // siparisID
+                strtok(NULL, "|"); // kullanıcı adı
                 char *yemekAdi = strtok(NULL, "|");
-                char *fiyat = strtok(NULL, ",|");
-                char *siparisZamani = strtok(NULL, "|");
-                char *hazirlanmaSuresi = strtok(NULL, "|");
-                printf("Siparis hazirlaniyor: ID: %s - Kullanici: %s - Yemek: %s - Hazirlanma Suresi: %s dakika\n", siparisID, kullaniciAdi, yemekAdi, hazirlanmaSuresi);
-
-                // Hazirda bir asci bul
+                strtok(NULL, "|"); // fiyat
+                strtok(NULL, "|"); // sipariş zamanı
+                int hazirlanmaSuresi = atoi(strtok(NULL, "|"));
+                
+                // Boşta aşçı bul
                 int i;
                 for (i = 0; i < ASCILAR; i++) {
-                    if (ascilar[i].hazirlanacakSure == 0) {
-                        // Asciya siparisi ata
-                        printf("Asci %d, %s siparisi hazirliyor. Tamamlanmasi icin %d dakika bekleyecek.\n", ascilar[i].id, yemekAdi, yemekler[i].hazirlanmaSuresi);
-                        ascilar[i].hazirlanacakSure = yemekler[i].hazirlanmaSuresi;
-                        fseek(dosya, -1 * strlen(satir), SEEK_CUR);
-                        fprintf(dosya, "2"); // Siparis durumu "hazirlandi" olarak degistir
+                    if (ascilar[i].hazirlanacakSure <= 0) {
+                        ascilar[i].hazirlanacakSure = hazirlanmaSuresi;
+                        strcpy(ascilar[i].yemek, yemekAdi); // Aşçının yemeğini kaydet
+                        printf("Asci %d, %s siparisi hazirliyor. Tamamlanmasi %d dakika.\n", ascilar[i].id, yemekAdi, hazirlanmaSuresi);
+
+                        // Sipariş durumunu güncelle (2: Hazırlanıyor)
+                        long konum = ftell(dosya);
+                        fseek(dosya, konum - strlen(satir), SEEK_SET);
+                        fprintf(dosya, "2");
+                        fflush(dosya); // Değişiklikleri hemen uygula
                         break;
                     }
                 }
 
                 if (i == ASCILAR) {
-                    printf("Tum ascilar mesgul. Biraz bekleniyor...\n");
-                    sleep(60);
+                    printf("Tum ascilar mesgul. Siparisler birazdan hazirlanacak...\n");
+                    sleep(5); // Kısa bir süre bekle ve tekrar dene
+                }
+            }
+        }
+
+        // Aşçıların sürelerini azalt
+        for (int i = 0; i < ASCILAR; i++) {
+            if (ascilar[i].hazirlanacakSure > 0) {
+                ascilar[i].hazirlanacakSure--;
+                if (ascilar[i].hazirlanacakSure == 0) {
+                    printf("Asci %d, %s siparisini tamamladi!\n", ascilar[i].id, ascilar[i].yemek);
                 }
             }
         }
 
         fclose(dosya);
+        sleep(60); // Her dakika kontrol et
     }
 }
 
 int main() {
     Yemek yemekler[YEMEK_UZUNLUK];
-    int yemekSayisi = 0;
-
-    siparisleriOkuyucu(yemekler, &yemekSayisi);
-
     Asci ascilar[ASCILAR];
+
+    yemekListesiniOku(yemekler);
+
     for (int i = 0; i < ASCILAR; i++) {
         ascilar[i].id = i + 1;
         ascilar[i].hazirlanacakSure = 0;
+        strcpy(ascilar[i].yemek, ""); // Başlangıçta boş yemek
     }
 
-    siparisleriIsleyici();
+    siparisleriIsle(yemekler, ascilar);
 
     return 0;
 }
