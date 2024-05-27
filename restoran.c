c22d0bb6a6e7af1b5d07bf3132d9bafd4f5dc4d8
miv403 <62180556+miv403@users.noreply.github.com>
2024-05-27 04:42:32 +0300

    restoran.c: yemek listsi güncelleme eklendi

--- restoran.c
+++ restoran.c
@@ -1,12 +1,104 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+#include <stdarg.h>
 #include "./csv.c"
 
-#define MAX_YEMEK_ADI_UZUNLUGU 100
-#define MAX_SATIR_UZUNLUGU 256
-#define YEMEK_LISTESI "./veri/yemeklistesi.txt"
-#define SIPARIS "./veri/siparisler.txt"
+#define MAX_SATIR_UZUNLUGU 4096
+#define YEMEK_LISTESI "./veri/yemekListesi.csv"
+#define SIPARIS "./veri/siparisler.csv"
+#define AD_MAX 50
+#define YEMEK_MAX 20
+
+typedef struct {
+    char ad[AD_MAX];
+    char fiyat[AD_MAX];
+    char sure[AD_MAX];
+    char durum[AD_MAX];
+}Yemek;
+
+void yeniSiparisleriGoster();
+void yemekListesiniGoster();
+void gunlukRaporOlustur(const char* kaynakDosya);
+size_t yemekListesiOku(FILE * dosya, Yemek yemekler[]);
+void yemekListesiSecenekler();
+void raporArayuzu();
+void raporGoruntule(const char* date);
+void csvYaz0(FILE *dosya,  int hucreSayisi, ... );
+
+int main() {
+    int secim;
+    char tarih[11];
+    int siparisID, yeniDurum;
+
+    while (1) {
+        printf("\nRestoran Yonetim Programi\n");
+        printf("1. Yemek Listesini Duzenle\n");
+        printf("2. Yeni Siparisleri Denetle\n");
+        printf("3. Gunluk Rapor Al\n");
+        printf("4. Analiz\n");
+        printf("5. Cikis\n");
+        printf("Seciminizi yapiniz: ");
+        scanf("%d", &secim);
+
+        switch (secim) {
+            case 1:
+                yemekListesiniGoster();
+                // TODO: ilgili yemek numarası ile ilgili satir numarasi istemi
+                yemekListesiSecenekler();
+                break;
+            case 2:
+                yeniSiparisleriGoster();
+                break;
+            case 3: // Gunluk Rapor 
+                raporArayuzu();
+                break;
+            case 4:
+                // ANALİZ
+                break;
+            case 5:
+                printf("Programdan cikis yapiliyor.\n");
+                exit(0);
+            default:
+                printf("Gecersiz secim. Lutfen tekrar deneyiniz.\n");
+        }
+    }
+
+    return 0;
+}
+
+void raporArayuzu() {
+    int secim;  // Kullan�c� se�imi i�in de�i�ken
+    char date[11];  // Tarihi tutmak i�in dizi
+
+    while (1) {  // S�rekli d�ng�, kullan�c� program� sonland�rana kadar devam eder
+        // Kullan�c�ya men� se�enekleri sunar
+        printf("1. Yeni rapor olustur\n");
+        printf("2. Gunluk rapor goruntulemek\n");
+        printf("3. Cikis\n");
+        printf("Secimini gir: ");
+        scanf("%d", &secim);  // Kullan�c� se�imini al�r
+
+        switch (secim) {  // Kullan�c� se�imine g�re i�lem yapar
+        case 1:
+            gunlukRaporOlustur(SIPARIS);  // G�nl�k raporu olu�turur
+            break;
+        case 2:
+            // Kullan�c�dan tarih al�r
+            printf("Tarihi gir (format: YYYY-MM-DD): ");
+            scanf("%s", date);  // Tarihi al�r
+            raporGoruntule(date);  // �lgili tarihe ait raporu g�r�nt�ler
+            break;
+        case 3:
+            exit(0);  // Program� sonland�r�r
+        default:
+            printf("Gecersiz secim.\n");  // Ge�ersiz se�im mesaj� verir
+        }
+    }
+    return;  // Program�n ba�ar�l� bir �ekilde bitti�ini belirtir
+
+}
 
 // Yeni siparisleri gosteren fonksiyon 
 void yeniSiparisleriGoster() {
@@ -31,27 +123,192 @@ void yeniSiparisleriGoster() {
                 "Asci");
     puts("---------------------------------------------------------------------------------------------------------");
     siparislerYazdir(siparisDosyasi, 150);
-
-/*
-    char satir[MAX_SATIR_UZUNLUGU];
-    while (fgets(satir, MAX_SATIR_UZUNLUGU, dosya) != NULL) {
-        char *siparisID = strtok(satir, "|");
-        char *kullaniciAdi = strtok(NULL, "|");
-        char *yemekAdi = strtok(NULL, "|");
-        char *fiyat = strtok(NULL, "|");
-        char *siparisZamani = strtok(NULL, "|");
-        char *hazirlanmaSuresi = strtok(NULL, "|");
-        char *durum = strtok(NULL, "\n");
-        if (strcmp(durum, "0") == 0) {
-            printf("ID: %s - Kullanici: %s - Yemek: %s - Fiyat: %s TL - Siparis Zamani: %s - Hazirlanma Suresi: %s dk\n", siparisID, kullaniciAdi, yemekAdi, fiyat, siparisZamani, hazirlanmaSuresi);
-        }
-    }
-*/
 }
 
+void yemekListesiSecenekler() {
+    void yemekEkle();
+    void yemekSil();
+    void yemekGuncelle();
+
+    puts("1. Yemek Ekle");
+    puts("2. Yemek Sil");
+    puts("3. Yemeği Düzenle");
+    int kullaniciSecim;
+    do{
+        printf("? ");
+        scanf("%d", &kullaniciSecim);
+    }while(kullaniciSecim < 1 || kullaniciSecim > 3);
+    
+
+    switch (kullaniciSecim) {
+    case 1:
+        yemekEkle();
+        break;
+    case 2:
+        yemekSil();
+        break;
+    case 3:
+        yemekGuncelle();
+        break;
+    default:
+        puts("Hatali secim.");
+        yemekListesiSecenekler(); //FIXME recursive iyi bir seçim olmayabilir
+        break;
+    }
+}
+
+void yemekEkle() {
+
+    Yemek yemek;
+
+    FILE * yemekListesi;
+    yemekListesi = fopen("./veri/yemekListesi.csv", "a");
+
+    if(yemekListesi == NULL) {
+        printf("Yemek Listesi dosyasi acilamadi.\n");
+        return;
+    }
+
+    printf("Yemek adi girin: ");
+    scanf("%50s", yemek.ad); // FIXME: boşluk karakter işi bozuyor. gets() derleme uyarısı veriyor.
+    printf("Fiyat girin (TL) girin: ");
+    scanf("%50s", yemek.fiyat);
+    printf("Hazirlanma suresi girin: ");
+    scanf("%50s", yemek.sure);
+    printf("Yemegin durumunu girin[Mevcut/Mevcut degil]: ");
+    scanf("%50s", yemek.durum);
+
+    if(!(strcmp(yemek.durum, "Mevcut"))) strcpy(yemek.durum, "True");
+    else                              strcpy(yemek.durum, "False");
+    csvYaz(yemekListesi, 4,
+            yemek.ad,
+            yemek.fiyat,
+            yemek.sure,
+            yemek.durum);
+    fclose(yemekListesi);
+}
+
+// TODO: yemek silme
+
+void yemekSil() {
+
+
+}
+
+// TODO: yemek güncelleme
+
+void yemekGuncelle() {
+
+    Yemek yemekler[YEMEK_MAX];
+
+    FILE * yemekListesi;
+    yemekListesi = fopen(YEMEK_LISTESI, "r");
+
+    size_t yemekSayisi = yemekListesiOku(yemekListesi,yemekler);
+
+    fclose(yemekListesi);
+
+    printf("Guncellemek istediginiz yemek adi: ");
+    char kullaniciYemekIN[AD_MAX];
+    
+    scanf("%49s", kullaniciYemekIN);
+
+    printf("%s a\n", kullaniciYemekIN);
+
+    puts("1. Yemek adi guncelle");
+    puts("2. Fiyat guncelle");
+    puts("3. Hazirlanma suresi guncelle");
+    puts("4. Durum guncelle");
+
+    int kullaniciSecim;
+    do{
+        printf("? ");
+        scanf("%d", &kullaniciSecim);
+    } while(kullaniciSecim < 1 || kullaniciSecim > 4);
+
+    char kullaniciGuncellemeIN[AD_MAX];
+    
+    
+    switch (kullaniciSecim)
+    {
+    case 1:
+        printf("Yeni ad girin: ");
+        scanf("%49s", kullaniciGuncellemeIN);
+        break;
+    case 2:
+        printf("Yeni fiyat girin: ");
+        scanf("%49s", kullaniciGuncellemeIN);
+        break;
+    case 3:
+        printf("Yeni sure girin: ");
+        scanf("%49s", kullaniciGuncellemeIN);
+        break;
+    case 4:
+        printf("Yeni durum girin[Mevcut/Mevcut Degil]: ");
+        scanf("%49s", kullaniciGuncellemeIN);
+        break;
+    default:
+        break;
+    }
+
+    for(size_t i = 0; i < yemekSayisi; ++i) {
+
+        if(!(strcmp(kullaniciYemekIN, yemekler[i].ad))) {
+            switch (kullaniciSecim) {
+                case 1:
+                    strcpy(yemekler[i].ad, kullaniciGuncellemeIN);
+                    break;
+                case 2:
+                    strcpy(yemekler[i].fiyat, kullaniciGuncellemeIN);
+                    break;
+                case 3:
+                    strcpy(yemekler[i].sure, kullaniciGuncellemeIN);
+                    break;
+                case 4:
+                    if(strcmp(kullaniciGuncellemeIN, "MevcutDegil") == 0) {
+                        
+                        strcpy(yemekler[i].durum, "False");
+
+                    } else if (strcmp(kullaniciGuncellemeIN, "Mevcut") == 0){
+
+                        strcpy(yemekler[i].durum, "True");
+                    }
+                    break;
+                default:
+                    printf("varsayilan durum yemek guncelle");
+                    break;
+            }       
+        }
+    }
+
+    FILE *yemekListeYENI;
+
+    yemekListeYENI = fopen("./veri/yemekListesi0.csv", "w");
+
+    fprintf(yemekListeYENI, "yemekAdi,yemekFiyati,hazirlanmaSuresi,durum\n");
+    // yemek listesini tekrar yazdir
+    for(size_t i = 0; i < yemekSayisi; ++i) {
+
+        csvYaz0(yemekListeYENI, 4,
+                yemekler[i].ad,
+                yemekler[i].fiyat,
+                yemekler[i].sure,
+                yemekler[i].durum);
+    }
+    fprintf(yemekListeYENI, "\n");
+    fclose(yemekListeYENI);
+
+    rename("./veri/yemekListesi0.csv",YEMEK_LISTESI);
+}
+
+
 // Yemek listesini gosteren fonksiyon
+
+
+
 void yemekListesiniGoster() {
     // Yemek listesi dosyasını acar, mevcut yemekleri listeler.
+
     FILE *dosya = fopen("./veri/yemekListesi.csv", "r");
     if (dosya == NULL) {
         printf("Yemek listesi dosyasi bulunamadi.\n");
@@ -66,124 +323,117 @@ void yemekListesiniGoster() {
     puts("-------------------------------------------------------"); // yeni satir
     yemekListesiYazdir(dosya, 150);
     fclose(dosya);
-    /*
-    printf("Mevcut Yemekler:\n");
-    char satir[MAX_SATIR_UZUNLUGU];
-    while (fgets(satir, MAX_SATIR_UZUNLUGU, dosya) != NULL) {
-        printf("%s", satir);
-    }
-    */
+
 }
 
-// Gunluk rapor alan fonksiyon
-void gunlukRaporAl(char* tarih) {
-    // Belirtilen tarihe ait siparisleri rapor dosyasina kaydeder.
-    char raporDosyaAdi[50];
-    sprintf(raporDosyaAdi, "gunluk_rapor_%s.txt", tarih);
+void gunlukRaporOlustur(const char* sourceFileName) {
 
-    FILE *siparisDosyasi = fopen(SIPARIS, "r");
-    if (siparisDosyasi == NULL) {
-        printf("Siparisler dosyasi bulunamadi.\n");
+    time_t now = time(NULL);  // Mevcut zaman bilgisini al�r (time_t zaman t�r�)
+    struct tm* t = localtime(&now);  // Zaman bilgisini yerel zamana �evirir (struct tm yap� t�r�)
+
+    char newFileName[64];  // Yeni dosya ad� i�in bir dizi tan�mlar
+    strftime(newFileName, sizeof(newFileName) - 1, "report_%Y-%m-%d.csv", t);  // Dosya ad�n� tarih ile olu�turur
+
+    FILE* sourceFile = fopen(sourceFileName, "r");  // Kaynak dosyay� okuma modunda a�ar ("r" modu, sadece okuma)
+    FILE* destFile = fopen(newFileName, "w");  // Hedef dosyay� yazma modunda a�ar ("w" modu, dosya yoksa olu�turur)
+
+    if (sourceFile == NULL || destFile == NULL) {
+        perror("Dosya acilamadi");  // Dosyalar a��lamazsa hata mesaj� verir
+        if (sourceFile != NULL) fclose(sourceFile);  // Kaynak dosya a��ksa kapat�r
+        if (destFile != NULL) fclose(destFile);  // Hedef dosya a��ksa kapat�r
         return;
     }
 
-    FILE *raporDosyasi = fopen(raporDosyaAdi, "w");
-    if (raporDosyasi == NULL) {
-        printf("Rapor dosyasi olusturulamadi.\n");
-        fclose(siparisDosyasi);
+    char buffer[256];  // Okuma ve yazma i�in bir buffer tan�mlar
+    while (fgets(buffer, sizeof(buffer), sourceFile) != NULL) {  // Dosyadan sat�r sat�r okur
+        fprintf(destFile, "%s", buffer);  // Veriyi hedef dosyaya yazar
+    }
+
+    fclose(sourceFile);  // Kaynak dosyay� kapat�r
+    fclose(destFile);  // Hedef dosyay� kapat�r
+    printf("Gunluk rapor olusturuldu: %s\n", newFileName);  // Ba�ar� mesaj� verir
+}
+
+// Belirli bir tarihe ait raporu g�r�nt�leyen fonksiyon
+void raporGoruntule(const char* date) {
+    char fileName[64];  // Dosya ad� i�in bir dizi tan�mlar
+    snprintf(fileName, sizeof(fileName), "report_%s.csv", date);  // Dosya ad�n� tarih ile olu�turur
+
+    FILE* file = fopen(fileName, "r");  // Dosyay� okuma modunda a�ar
+    if (file == NULL) {
+        perror("Dosya acilamadi");  // Dosya a��lamazsa hata mesaj� verir
         return;
     }
 
-    fprintf(raporDosyasi, "Gunluk Rapor (%s):\n", tarih);
-    fprintf(raporDosyasi, "----------------------------------------\n");
+    printf("\n%-15s%-15s%-10s%-20s%-20s%-15s%-10s\n",
+            "Siparis ID",
+            "Yemek Adi",
+            "Fiyat (TL)",
+            "Siparis Zamani",
+            "Hazirlanma Zamani",
+            "Kullanici Adi",
+            "Asci");
+    printf("-----------------------------------------------------------\n");
 
     char satir[MAX_SATIR_UZUNLUGU];
-    while (fgets(satir, MAX_SATIR_UZUNLUGU, siparisDosyasi) != NULL) {
-        char *siparisTarihi = strtok(satir, ",");
-        if (strstr(siparisTarihi, tarih) != NULL) {
-            fprintf(raporDosyasi, "%s", satir);
+    fgets(satir, sizeof(satir), file);
+    while (fgets(satir, sizeof(satir), file) != NULL) {  // Dosyan�n sonuna kadar okur
+        char* siparisID = strtok(satir, ",");
+        char* yemekAdi = strtok(NULL, ",");
+        char* fiyat = strtok(NULL, ",");
+        char* siparisZamani = strtok(NULL, ",");
+        char *hazirlanmaZamani = strtok(NULL, ",");
+        char* kullaniciAdi = strtok(NULL, ",");
+        char* asci = strtok(NULL, "\n");
+
+        if (!siparisID || !yemekAdi || !fiyat || !siparisZamani || !hazirlanmaZamani || !kullaniciAdi || !asci) {
+            fprintf(stderr, "%s\n", satir);
+            continue;  // Hatal� sat�rlar� atlar
         }
+
+        printf("%-15s%-15s%-10s%-20s%-20s%-15s%-10s\n",
+                siparisID,
+                yemekAdi,
+                fiyat,
+                siparisZamani,
+                hazirlanmaZamani,
+                kullaniciAdi,
+                asci);
     }
-
-    printf("Gunluk rapor '%s' adli dosyaya kaydedildi.\n", raporDosyaAdi);
-
-    fclose(siparisDosyasi);
-    fclose(raporDosyasi);
+    fclose(file);  // Dosyay� kapat�r
 }
+size_t yemekListesiOku(FILE * dosya, Yemek yemekler[]) {
 
-// Siparis durumunu guncelleyen fonksiyon
-void siparisDurumunuGuncelle(int siparisID, int yeniDurum) {
-    // Belirtilen siparisin durumunu gunceller
-    FILE *dosya = fopen(SIPARIS, "r+");
-    if (dosya == NULL) {
-        printf("Siparisler dosyasi bulunamadi.\n");
-        return;
+    // yemek listesi dosyasından birinci(ad) ve üçüncü(hazirlanmaSuresi) sütunu okur.
+
+    // yemek listesi dosyasından tüm verileri okur ve yemekler[] kaydeder
+    char satir[256];
+    int yemekSayisi = 0;
+
+    fgets(satir, 256, dosya);
+
+    for(int i = 0; fgets(satir, 256, dosya) != NULL && i < YEMEK_MAX; ++i) {
+
+        // , karakterine göre satırdan gerekli sütunları okuma
+
+        sscanf(satir, "%[^,] , %[^,] , %[^,] , %[^,]", yemekler[i].ad, yemekler[i].fiyat, yemekler[i].sure, yemekler[i].durum);
+
+        yemekSayisi++;
     }
-
-    char satir[MAX_SATIR_UZUNLUGU];
-    long dosyaBaslangicKonumu;
-    int bulunanID;
-    while (fgets(satir, MAX_SATIR_UZUNLUGU, dosya) != NULL) {
-        dosyaBaslangicKonumu = ftell(dosya);
-        char *siparisIDStr = strtok(satir, "|");
-        bulunanID = atoi(siparisIDStr);
-        if (bulunanID == siparisID) {
-            fseek(dosya, dosyaBaslangicKonumu, SEEK_SET);
-            fprintf(dosya, "%d", yeniDurum);
-            break;
-        }
-    }
-
-    fclose(dosya);
-
-    if (bulunanID != siparisID) {
-        printf("Belirtilen ID numarasina sahip bir siparis bulunamadi.\n");
-    } else {
-        printf("Siparis durumu basariyla guncellendi.\n");
-    }
+    return yemekSayisi;
 }
+void csvYaz0(FILE *dosya,  int hucreSayisi, ... ) {
 
-int main() {
-    int secim;
-    char tarih[11];
-    int siparisID, yeniDurum;
+    va_list args;
+    va_start(args, hucreSayisi);
 
-    while (1) {
-        printf("\nRestoran Yonetim Programi\n");
-        printf("1. Mevcut Yemekleri Goster\n");
-        printf("2. Yeni Siparisleri Goster\n");
-        printf("3. Gunluk Rapor Al\n");
-        printf("4. Siparis Durumunu Guncelle\n");
-        printf("5. Cikis\n");
-        printf("Seciminizi yapiniz: ");
-        scanf("%d", &secim);
-
-        switch (secim) {
-            case 1:
-                yemekListesiniGoster();
-                break;
-            case 2:
-                yeniSiparisleriGoster();
-                break;
-            case 3:
-                printf("Gunluk rapor almak istediginiz tarihi (YYYY-MM-DD formatinda) giriniz: ");
-                scanf("%s", tarih);
-                gunlukRaporAl(tarih);
-                break;
-            case 4:
-                printf("Guncellenecek siparisin ID numarasini giriniz: ");
-                scanf("%d", &siparisID);
-                printf("Yeni durumu giriniz (0: Beklemede, 1: Hazirlaniyor, 2: Teslim Edildi): ");
-                scanf("%d", &yeniDurum);
-                siparisDurumunuGuncelle(siparisID, yeniDurum);
-                break;
-            case 5:
-                printf("Programdan cikis yapiliyor.\n");
-                exit(0);
-            default:
-                printf("Gecersiz secim. Lutfen tekrar deneyiniz.\n");
-        }
+    for (int i = 0; i < hucreSayisi - 1; ++i) {
+        char *str = va_arg(args, char *);
+        fprintf(dosya, "%s,", str);
     }
+    char *str = va_arg(args, char *);
+    fprintf(dosya, "%s", str);
+    //fprintf(dosya, "\n");
 
-    return 0;
+    va_end(args);
 }
